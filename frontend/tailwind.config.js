cda137550d24c0be9754e34ed76147a528b0c87a
FelipeArce <arceprogramando@gmail.com>
2025-01-18 13:06:15 -0300

    fix : tailwind content

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src//*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        gris: "#737072",
-        azulOscuro: "#5F95D7",
-        celesteClaro: "#C1DDFF",
-        amarilloOscuro: "#FDB900",
-        amarilloClaro: "#FFEEC1",
-        amarilloLogo: "#F6E77C",
-        cremaClaro: "#FFFAED",
+        gris: '#737072',
+        azulOscuro: '#5F95D7',
+        celesteClaro: '#C1DDFF',
+        amarilloOscuro: '#FDB900',
+        amarilloClaro: '#FFEEC1',
+        amarilloLogo: '#F6E77C',
+        cremaClaro: '#FFFAED',
       },
     },
   },
