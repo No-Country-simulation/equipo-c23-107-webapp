62390b47812b849e5c2fa2c2985fc4fe7a076c6f
FelipeArce <arceprogramando@gmail.com>
2025-01-20 01:47:28 -0300

    fix: change 'around' to 'between' in flex justification and add padding

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
-    <header className="flex justify around">
-      <p>Idiomas</p>
+    <header className="flex justify-between p-2 items-center">
+      <p className="border-2 px-4 rounded">Idiomas</p>
       <div className="flex gap-2">
-        <p className="bg-azulOscuro py-2 text-white min-w-44 rounded-lg">Registro</p>
-        <p className="bg-celesteClaro py-2 text-black min-w-44 rounded-lg border-2 border-azulOscuro">Iniciar Sesión</p>
+        <p className="text-center bg-azulOscuro py-2 text-white min-w-44 rounded-lg">Registro</p>
+        <p className="text-center bg-celesteClaro py-2 text-black min-w-44 rounded-lg border-2 border-azulOscuro">Iniciar Sesión</p>
       </div>
     </header>
   );
