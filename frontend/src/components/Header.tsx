495390918e735f102e276ceb7b44152f71fc113c
FelipeArce <arceprogramando@gmail.com>
2025-01-23 01:22:27 -0300

    feat: add login and registration pages, and update routes in App

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,22 +1,25 @@
 import { useStore } from "exome/react";
 import { authStore } from "../core/store/AuthStore";
 import LoginModal from "../features/LoginModal/LoginModal";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { modalOpen, setModalOpen } = useStore(authStore);
   return (
     <header className="flex flex-1 justify-between p-2 items-center">
-      { modalOpen ? <LoginModal /> : null }
+      {modalOpen ? <LoginModal /> : null}
       <p className="border-2 px-4 rounded">Idiomas</p>
       <div className="flex gap-2">
-        <button className="text-center bg-azulOscuro py-2 text-white min-w-44 rounded-lg cursor-pointer"
-          onClick={ () => setModalOpen(!modalOpen) }
-        >
-          Registro
+        <button className="text-center bg-azulOscuro py-2 text-white min-w-44 rounded-lg cursor-pointer" onClick={() => setModalOpen(!modalOpen)}>
+          Probar Inicio
         </button>
-        <p className="text-center bg-celesteClaro py-2 text-black min-w-44 rounded-lg border-2 border-azulOscuro cursor-pointer">
+
+        <Link to="/register" className="text-center bg-azulOscuro py-2 text-white min-w-44 rounded-lg">
+          Registro
+        </Link>
+        <Link to="/login" className="text-center bg-celesteClaro py-2 text-black min-w-44 rounded-lg border-2 border-azulOscuro">
           Iniciar Sesión
-        </p>
+        </Link>
       </div>
     </header>
   );
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import Layout from "../layouts/Layout";
+
+const Register = () => {
+  return (
+    <Layout>
+      <h2>Register</h2>
+    </Layout>
+  );
+};
+
+export default Register;
