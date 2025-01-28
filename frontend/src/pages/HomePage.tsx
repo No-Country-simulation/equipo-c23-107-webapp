25f1060f47a9fa7764969fdb5b7d1f44441ca4d9
Felipe Arce <arceprogramando@gmail.com>
2025-01-28 18:30:40 -0300

    feat: update homepage content and center the Google Sign-In button.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,9 @@ import Layout from '../layouts/Layout';
 const HomePage = () => {
   return (
     <Layout>
-      <div className="underline">HomePage</div>
+      <p>Bienvenidos a <span className='font-bold'>Las Recetas de la Nona!</span></p>
+      <p>Un espacio donde el amor por la cocina y los recuerdos familiares se entrelazan. Cada receta aquí es un tesoro, una ventana al legado culinario de nuestras abuelas que, con sus manos y corazon alimentaron generaciones.</p>
+      <p>Por que cada receta tiene una historia y cada historia merece ser compartida. ¡Sumate a ser parte de esta tradición!</p>
     </Layout>
   );
 };
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -17,9 +17,9 @@ const Login = () => {
   return (
     <Layout>
       <div className="flex items-center justify-center min-h-screen">
-        <div className="w-full max-w-md p-8  rounded-lg  border-2 border-black bg-white">
+        <div className="flex flex-col justify-center  max-w-md p-8  rounded-lg  border-2 border-black bg-white">
           <h2 className="mb-6 text-lg font-bold text-center text-gray-800">Ingresa tus datos para iniciar sesión:</h2>
-          <form className="flex flex-col items-center space-y-4" onSubmit={ handleSubmit }>
+          <form className="flex flex-col items-center space-y-4" onSubmit={handleSubmit}>
             <div className="max-w-72">
               <div className="w-full">
                 <label htmlFor="email" className="block text-lg font-bold text-black">
@@ -65,20 +65,24 @@ const Login = () => {
               </button>
             </div>
           </form>
-          <button
-            type="button"
-            onClick={ loginWithGoogle }
-            className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  me-2 mb-2"
-          >
-            <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-              <path
-                fill-rule="evenodd"
-                d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <p>Iniciar Sesión con <span className="underline">Google</span></p>
-          </button>
+          <div className="flex justify-center items-center ">
+            <button
+              type="button"
+              onClick={loginWithGoogle}
+              className="flex justify-center bg-azulOscuro text-white  hover:bg-[#4285F4]/90  focus:outline-none  font-medium rounded-lg text-sm max-w-56 px-4 py-2.5 text-cente "
+            >
+              <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                <path
+                  fill-rule="evenodd"
+                  d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <p>
+                Iniciar Sesión con <span className="underline">Google</span>
+              </p>
+            </button>
+          </div>
         </div>
       </div>
     </Layout>
