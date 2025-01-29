f2623dc7e839b36be62b5afc16576ac30232e04c
Gus - MVC <gustavotiseira@gmail>
2025-01-29 08:30:47 -0300

    feat: Add email and update token, profile image, and createdAt in user entity

--- backend/src/features/User/user.entity.ts
+++ backend/src/features/User/user.entity.ts
@@ -1,22 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "varchar", length: 100 })
+  @Column({ type: "varchar", length: 100, unique: true })
   uid: string;
 
   @Column({ type: "varchar", length: 100 })
   name: string;
 
-  @Column({ type: "varchar", length: 100 })
+  @Column({ type: "varchar", length: 150, unique: true })
+  email: string;
+
+  @Column({ type: "text" })
   token: string;
 
-  @Column({ type: "varchar", length: 100 })
+  @Column({ type: "varchar", length: 255, nullable: true })
   profileImage: string;
 
-  @Column({ type: "varchar", length: 255 })
-  createdAt: string;
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt: Date;
 }
