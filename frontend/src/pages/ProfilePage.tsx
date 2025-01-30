00dd360ace2eaa7aa499e973dd815a20a22765c8
Felipe Arce <arceprogramando@gmail.com>
2025-01-30 16:39:54 -0300

    feat: update ProfilePage to display mockup user information and remove logout button

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,15 +1,27 @@
-import Layout from "../layouts/Layout";
-import { useStore } from "exome/react";
-import { authStore } from "../core/store/AuthStore";
+import Layout from '../layouts/Layout';
+
+const user = {
+  name: 'Sarah Johnson',
+  imgThumbnail: 'SarahJohnson.png',
+  description: 'Cada receta tiene su magia: es un pedacito de historia que vuelve a cobrar vida en la mesa.',
+  countRecipes: 4,
+  country: 'Uruguay',
+};
 
 const ProfilePage = () => {
-  const { logout } = useStore(authStore);
   return (
     <Layout>
-      <h1>Profile Page</h1>
-      <button onClick={ logout }>Cerrar sessión</button>
+      {user && (
+        <div className='-mt-16 '>
+          <img src={user.imgThumbnail} alt={user.name} className='mx-auto' />
+          <h1>{user.name}</h1>
+          <p>{user.description}</p>
+          <p>{user.countRecipes} recetas</p>
+          <p>{user.country}</p>
+        </div>
+      )}
     </Layout>
-  )
+  );
 };
 
 export default ProfilePage;
